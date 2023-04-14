fa1411b622e2a0bb006a15157a8ef73ee6d9d551
Maurice Künicke <m.kuenicke1995@gmail.com>
2023-04-14 23:21:00 +0200

    add healthcheck endpoint for coverage

--- calypso/main.py
+++ calypso/main.py
@@ -2,13 +2,18 @@ from fastapi import FastAPI, Depends
 from .middleware import log_requests_middleware
 from calypso.modules.logger import CalypsoLogger
 from calypso import deps
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 app.middleware("http")(log_requests_middleware)
 
 
-@app.get("/")
-def read_root(rid: str = Depends(deps.extract_request_id)) -> dict[str, str]:
-    """Test Endpoint."""
-    CalypsoLogger.debug("Hello World!", extra={"idem": rid, "mod": __name__})
-    return {"Hello": "World"}
+@app.get("/healthcheck", include_in_schema=False)
+def read_root(rid: str = Depends(deps.extract_request_id)) -> JSONResponse:
+    """Healthcheck Endpoint."""
+    CalypsoLogger.info("Healthcheck received.", extra={"idem": rid, "mod": __name__})
+    return JSONResponse(
+        content={"status": "available"},
+        headers={"cache-control": "no-cache"},
+        status_code=200,
+    )
